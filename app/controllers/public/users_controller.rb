fe321c191530d0d98760be068b7d7a13ce619481
Tatsuya Miyagawa <zgmf326@gmail.com>
2024-12-05 02:44:40 +0000

    [add]header,footer

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,16 +1,35 @@
 class Public::UsersController < ApplicationController
   def mypage
+    @user = current_user
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to mypage_path, notice: "ユーザー情報が更新されました"
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to root_path, notice: "退会しました"
   end
+
+  private
+
+ def user_params
+   params.require(:user),permit(:name :introduction)
+ end
+
 end
