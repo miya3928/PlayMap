da0164c3edd467501016e3ff33f9e91aa210d5f1
Tatsuya Miyagawa <zgmf326@gmail.com>
2024-12-05 01:37:53 +0000

    コントローラーの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For deta"ils on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ 
+  
+  scope module: :public do
+     root to:"homes#top"
+  get "/about", to:"homes#about"
+  get "/mypage", to:"users#mypage"
+  resources :users, only:[:create,:show,:edit,:update]
+  resources :posts
+  end
 end
