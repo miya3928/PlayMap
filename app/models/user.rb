0d08bf5ccef7df799251cda7c9ebddb0afe2533c
Tatsuya Miyagawa <zgmf326@gmail.com>
2024-12-12 08:24:22 +0000

    [fix]user.rb,コメントアウト解除

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
 
-  # devise :database_authenticatable, :registerable,
-  #        :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 
-  # validates :email, presence: true, uniqueness: true
-  # validates :password, presence: true, length: {minimum: 6 }, on:create ,unless: -> { guest_user? } 
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: {minimum: 6 }, on:create ,unless: -> { guest_user? } 
 
 GUEST_USER_EMAIL = "guest@example.com"
 
