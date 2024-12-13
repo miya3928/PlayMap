270c232ab71f89df2f5d825a767b5ffa4cd0e066
Tatsuya Miyagawa <zgmf326@gmail.com>
2024-12-13 01:50:33 +0000

    [add]テストデータ2

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,4 +15,19 @@ lucas = User.find_or_create_by!(email: "lucas@example.com") do |user|
   user.password = "password"
 end
 
+Post.find_or_create_by!(title: "Cavello") do |post|
+  post.body = "大人気のカフェです。"
+  post.user = olivia
+end
+
+Post.find_or_create_by!(title: "和食屋 百花繚乱") do |post|
+  post.body = "和食は美味しい！店内も広くて子供向けでした！"
+  post.user = james
+end
+
+Post.find_or_create_by!(title: "ShoreditchBar") do |post|
+  post.body = "メキシコ料理好きにおすすめ！子供と行くには合わないかも。。"
+  post.user = lucas
+end
+
 puts "seedの実行が完了しました"
