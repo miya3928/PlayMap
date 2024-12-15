68ba3d32e22535896005ad7e10d0a6e770cfe833
Tatsuya Miyagawa <zgmf326@gmail.com>
2024-12-15 15:52:21 +0000

    [add]place/new

--- app/controllers/public/events_controller.rb
+++ app/controllers/public/events_controller.rb
@@ -1,2 +1,31 @@
 class Public::EventsController < ApplicationController
+  def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+    @places = @event.places
+  end
+
+  def new
+    @event = Event.new
+    @places = Place.all
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to @event, notice: 'イベントを登録しました'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :body, :start_date, :end_date, place_ids: [])
+  end
 end
+
--- app/controllers/public/places_controller.rb
+++ app/controllers/public/places_controller.rb
@@ -3,5 +3,27 @@ class Public::PlacesController < ApplicationController
     @places = Place.all
   end
   
-  def 
+  def show
+    @place = Place.find(params[:id])
+    @events = @place.events
+  end
+  
+  def new
+    @place = Place.new
+  end
+  
+  def create
+    @place =Place.new(place_params)
+    if @place.save
+      redirect_to @place, notice: "場所を登録しました。"
+    else
+      render :new
+    end
+  end
+  
+ private
+
+  def place_params
+    params.require(:place).permit(:name, :address, :description, :latitude, :longitude)
+
 end
