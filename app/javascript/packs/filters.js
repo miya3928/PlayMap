9fcf0296500465e255180a1977bf09efa9f379fb
Tatsuya Miyagawa <zgmf326@gmail.com>
2025-02-25 09:37:17 +0000

    Google Mapの表示

--- app/javascript/packs/filters.js
+++ app/javascript/packs/filters.js
@@ -1,5 +1,4 @@
 document.addEventListener("turbolinks:load", function() {
-  console.log("filters.js loaded");
   const filterForm = document.getElementById("filter-form");
   if (!filterForm) return;
 
