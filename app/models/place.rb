ebc757b228ac25d3194fa7cf1e67893917935117
Tatsuya Miyagawa <zgmf326@gmail.com>
2025-04-07 08:31:33 +0000

    [add]placeテーブルにカラム追加

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,5 @@
 class Place < ApplicationRecord
+  has_one_attached :image
   has_many :event_places, dependent: :destroy
   has_many :events, through: :event_places
   has_many :posts, as: :postable, dependent: :destroy
@@ -8,4 +9,8 @@ class Place < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: ->(obj){ obj.address.present? && obj.address_changed? }
 
+
+    def full_address
+      "#{prefecture} #{city} #{street}"
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_02_12_022751) do
+ActiveRecord::Schema.define(version: 2025_04_07_074230) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -89,6 +89,10 @@ ActiveRecord::Schema.define(version: 2025_02_12_022751) do
     t.float "longitude"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "prefecture"
+    t.string "zipcode"
+    t.string "city"
+    t.string "street"
   end
 
   create_table "post_tags", force: :cascade do |t|
