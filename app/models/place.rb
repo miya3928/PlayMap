e51304869c4ac4da2f4c852f5e87f889ff438995
Tatsuya Miyagawa <zgmf326@gmail.com>
2025-04-10 06:05:11 +0000

    [fix]地図のマーカー表示

--- app/models/place.rb
+++ app/models/place.rb
@@ -4,7 +4,7 @@ class Place < ApplicationRecord
   has_many :events, through: :event_places
   has_many :posts, as: :postable, dependent: :destroy
   
-  validates :name, :address, :description, presence: true 
+  validates :name, :prefecture, :city, :street, :description, presence: true 
 
   geocoded_by :address
   after_validation :geocode, if: ->(obj){ obj.address.present? && obj.address_changed? }
