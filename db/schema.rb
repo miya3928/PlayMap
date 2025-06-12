1f28d7268a27bb997726399e620c8522d896605d
Tatsuya Miyagawa <zgmf326@gmail.com>
2025-06-12 07:58:10 +0000

    場所検索機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_04_07_074230) do
+ActiveRecord::Schema.define(version: 2025_06_12_034604) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -93,6 +93,7 @@ ActiveRecord::Schema.define(version: 2025_04_07_074230) do
     t.string "zipcode"
     t.string "city"
     t.string "street"
+    t.integer "prefecture_code"
   end
 
   create_table "post_tags", force: :cascade do |t|
