a19ad83d2f448219f031489bf8e1d5ff29586fcf
Tatsuya Miyagawa <zgmf326@gmail.com>
2025-09-07 07:23:08 +0000

    [add]通知テーブルの作成

---
+++ db/migrate/20250821024031_create_notifications.rb
@@ -0,0 +1,15 @@
+class CreateNotifications < ActiveRecord::Migration[6.1]
+  def change
+    create_table :notifications do |t|
+      t.integer :visitor_id
+      t.integer :visited_id
+      t.integer :post_id
+      t.integer :comment_id
+      t.integer :review_id
+      t.string :action
+      t.boolean :checked
+
+      t.timestamps
+    end
+  end
+end
